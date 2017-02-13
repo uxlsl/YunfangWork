116d2207c2e29155f5cb84b93689eb9cb0e96a32
codingEnzo <1012137875@qq.com>
2017-02-13 14:27:44 +0800

    update

--- fang_info_test/fang_info_test/settings.py
+++ fang_info_test/fang_info_test/settings.py
@@ -112,7 +112,7 @@ USER_AGENTS = [
   "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50",
   "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 2.0.50727; SLCC2; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.3; .NET4.0C; Tablet PC 2.0; .NET4.0E)"]
 
-REDIS_HOST = '192.168.6.4'
+REDIS_HOST = '192.168.10.73'
 REDIS_PORT = 6379
 
 # Enable and configure the AutoThrottle extension (disabled by default)
