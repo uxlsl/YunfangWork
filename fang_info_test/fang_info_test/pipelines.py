316189f9e5fbd9c5dea1e6b9fb8b2f0c40de0a8d
codingEnzo <1012137875@qq.com>
2017-02-16 15:01:09 +0800

    sss
    Signed-off-by: codingEnzo <1012137875@qq.com>

--- fang_info_test/fang_info_test/pipelines.py
+++ fang_info_test/fang_info_test/pipelines.py
@@ -12,46 +12,46 @@ from celery import Celery
 
 class Fang_Info_Test_Pipeline(object):
 
-	SVN_DATA_PATH = '/home/chiufung/fang_html/'
+    SVN_DATA_PATH = '/home/chiufung/fang_html/'
 
-	def __init__(self):
+    def __init__(self):
 
-		self.celery = Celery(broker="redis://192.168.6.4/2",backend="redis://192.168.6.4/3")
-		
-	def process_item(self, item, spider):
-		
-		host_name = 'fang'
-		url = item['url']
-		fang_id = item['fang_id']
-		region_code = '010'
-		body = item['body']
-		version_time = item['version_time']
-		source_route = item['source_route']
+        self.celery = Celery(broker="redis://192.168.6.4/2",backend="redis://192.168.6.4/3")
+        
+    def process_item(self, item, spider):
+        
+        host_name = 'fang'
+        url = item['url']
+        fang_id = item['fang_id']
+        region_code = '010'
+        body = item['body']
+        version_time = item['version_time']
+        source_route = item['source_route']
 
-		file_path = self.SVN_DATA_PATH
-		file_name = '{0}_{1}_{2}_{3}.html'.format(host_name,region_code,fang_id,version_time)
+        file_path = self.SVN_DATA_PATH
+        file_name = '{0}_{1}_{2}_{3}.html'.format(host_name,region_code,fang_id,version_time)
 
-		try:
+        try:
 
-			f = open(file_path+file_name,'w')
-			f.write(body)
-			f.close()
+            f = open(file_path+file_name,'w')
+            f.write(body)
+            f.close()
 
-			source_addr = '192.168.6.204'
-			source_port = 22
-			user_name = 'chiufung'
-			passwd = 'enzowcf'
-			file_path = file_path
-			file_name = file_name
-			self.celery.send_task("async_html_update_service.upload_service.upload_new_file",
+            source_addr = '192.168.6.204'
+            source_port = 22
+            user_name = 'chiufung'
+            passwd = 'enzowcf'
+            file_path = file_path
+            file_name = file_name
+            self.celery.send_task("async_html_update_service.upload_service.upload_new_file",
                     args=[source_addr,source_port,user_name,passwd,file_path,file_name,version_time,source_route],
                     queue='upload_queue')
-			log_mysql.log_in_mysql(response.url,'Crawl success')
+            log_mysql.log_in_mysql(response.url,'Crawl success')
             logger.debug('Crawl success')
-		except Exception as e:
-			print Exception,":",e
+        except Exception as e:
+            print Exception,":",e
 
-		return item
+        return item
 
-	#def spider_closed(self,spider):
-	#	self.file.close()
+    #def spider_closed(self,spider):
+    #    self.file.close()
