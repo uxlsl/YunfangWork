0e77e12fde1048dc7b3499ee20cf0e684c759700
lsl <ux_lsl@163.com>
2017-03-24 15:19:58 +0800

    修改中介统计脚本

--- developer_data_push/developer_data/house/stat.py
+++ developer_data_push/developer_data/house/stat.py
@@ -1,14 +1,17 @@
 # -*- coding:utf-8 -*-
-
+import pandas as pd
 from django_pandas.io import read_frame
-
 from house.models import (
-#        HouseofferforsaleHistory,
+        HouseofferforsaleHistory,
         HouseofferforsaleHistoryCopy,
-        ResidentialareaAvgPrice)
+        ResidentialareaAvgPrice,
+        AgentStat
+        )
 
 
 def residential_stat():
+    """小区均价统计
+    """
     df = read_frame(HouseofferforsaleHistoryCopy.objects.filter(housetype='普通公寓').only(
         'city',
         'casetime',
@@ -26,3 +29,32 @@ def residential_stat():
                 residentialareaid=k[3],
                 defaults={'junJia':v}
                 )
+
+
+def agent_stat():
+    df = read_frame(HouseofferforsaleHistory.objects.all())
+    def f(x):
+        return pd.Series({
+            'agentstore_count': len(x['agentstores'].unique()),
+            'agentname_count': len(x['agentname'].unique()),
+            'house_count': len(x)})
+    total = 0
+    results = df.groupby(['casetime', 'city', 'districtname','residentialareaname','agency', 'unitshape']).apply(f)
+    for index, row in results.iterrows():
+        item = []
+        item.extend(index)
+        item.extend([row['agentstore_count'], row['agentname_count'], row['house_count']])
+        AgentStat.objects.update_or_create(
+                ShiJian=item[0],
+                ChengShi=item[1],
+                XingZhengQu=item[2],
+                PianQu=item[3],
+                ZhongJieMingCheng=item[4],
+                YeTai=item[5],
+                defaults={
+                    'MenDianShu':item[6],
+                    'JingJiRenShu':item[7],
+                    'XinZengGongYingShu':item[8],
+                    })
+
+    return total,df.count()
--- developer_data_push/developer_data/scripts/agent_stat.py
+++ developer_data_push/developer_data/scripts/agent_stat.py
@@ -1,35 +1,6 @@
 # -*- coding:utf-8 -*-
-import pandas as pd
-from django_pandas.io import read_frame
-from house.models import  HouseofferforsaleHistory, AgentStat
+from house.stat import agent_stat
 
 
 def run():
-    df = read_frame(HouseofferforsaleHistory.objects.all())
-    def f(x):
-        return pd.Series({
-            'agentstore_count': len(x['agentstores'].unique()),
-            'agentname_count': len(x['agentname'].unique()),
-            'house_count': len(x)})
-    total = 0
-    results = df.groupby(['casetime', 'city', 'districtname','residentialareaname','agency', 'unitshape']).apply(f)
-    for index, row in results.iterrows():
-        print('#'*30)
-        item = (*index, row['agentstore_count'], row['agentname_count'], row['house_count'])
-        AgentStat.objects.update_or_create(
-                ShiJian=item[0],
-                ChengShi=item[1],
-                XingZhengQu=item[2],
-                PianQu=item[3],
-                ZhongJieMingCheng=item[4],
-                YeTai=item[5],
-                defaults={
-                    'MenDianShu':item[6],
-                    'JingJiRenShu':item[7],
-                    'XinZengGongYingShu':item[8],
-                    })
-        print(item)
-        print('#'*30)
-
-    print(df.count())
-    print(total)
+    agent_stat()
