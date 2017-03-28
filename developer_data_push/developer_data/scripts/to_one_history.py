e21ab4ab18b003283af7d244a4602893f8e3915f
lsl <ux_lsl@163.com>
2017-03-28 14:37:17 +0800

    增加一个汇总的数据

---
+++ developer_data_push/developer_data/scripts/to_one_history.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+
+from django.db import connection
+
+sql = """
+INSERT INTO `houseofferforsale_history_copy` (`Tid`, `City`, `HouseName`, `DistrictName`, `SpecialFactor`, `ResidentialAreaName`, `Address`, `CaseTime`, `CaseFrom`, `SourceID`, `SourceMeta`, `SourceLink`, `SourceTitle`, `Supplementary`, `ExtData`, `BuildingCategory`, `BuildingCompletedYear`, `RemainYear`, `BuildingArea`, `InsideOfBuildingArea`, `Toward`, `Decoration`, `HouseNature`, `HouseType`, `RealUseType`, `SupportingFacility`, `UnitPrice`, `TotalPrice`, `CategoryofProperty`, `ZoneCaseFrom`, `Partition`, `Dwelling`, `HouseTags`, `UnitShape`, `UnitStructure`, `Rooms`, `Halls`, `Kitchens`, `Toilets`, `Balconys`, `Agency`, `CorrespondaAgency`, `AgentName`, `AgentTelephone`, `AgentStores`, `CorrespondaAgentStores`, `HousesPictures`, `FloorName`, `Floor`, `Floors`, `FloorCount`, `FloorType`, `SharedPublicArea`, `UnitStructureFlag`, `HouseTypeFlag`, `LastCaseTime`, `CaseCount`, `Comment`, `ResidentialAreaID`, `BuildingID`, `UnitID`, `HouseID`, `ModifyDate`, `TGuid`, `PID`, `Expansion`, `CreatedDate`, `Status`, `remark`, `oppositeId`, `crawlAt`, `housingDataType`, `HouseStatus`, `datadate`)
+
+SELECT `Tid`, `City`, `HouseName`, `DistrictName`, `SpecialFactor`, `ResidentialAreaName`, `Address`, `CaseTime`, `CaseFrom`, `SourceID`, `SourceMeta`, `SourceLink`, `SourceTitle`, `Supplementary`, `ExtData`, `BuildingCategory`, `BuildingCompletedYear`, `RemainYear`, `BuildingArea`, `InsideOfBuildingArea`, `Toward`, `Decoration`, `HouseNature`, `HouseType`, `RealUseType`, `SupportingFacility`, `UnitPrice`, `TotalPrice`, `CategoryofProperty`, `ZoneCaseFrom`, `Partition`, `Dwelling`, `HouseTags`,
+`UnitShape`, `UnitStructure`, `Rooms`, `Halls`, `Kitchens`, `Toilets`, `Balconys`, `Agency`, `CorrespondaAgency`, `AgentName`, `AgentTelephone`, `AgentStores`, `CorrespondaAgentStores`, `HousesPictures`, `FloorName`, `Floor`, `Floors`, `FloorCount`, `FloorType`, `SharedPublicArea`, `UnitStructureFlag`, `HouseTypeFlag`, `LastCaseTime`, `CaseCount`, `Comment`, `ResidentialAreaID`, `BuildingID`, `UnitID`, `HouseID`, `ModifyDate`, `TGuid`, `PID`, `Expansion`, `CreatedDate`, `Status`, `remark`,
+`oppositeId`, `crawlAt`, `housingDataType`, `HouseStatus`, `datadate` FROM `{}`
+"""
+
+def run():
+    lst = [
+            'houseofferforsale_history_beijing',
+            'houseofferforsale_history_tianjin',
+            'houseofferforsale_history_zhengzhou'
+            ]
+
+    cursor = connection.cursor()
+
+    for i in lst:
+        cursor.execute(sql.format(i))
